3691466fd3c5c9cc094f27d38bffc105e62f1ec2
Giuliano Sposito <gsposito@ciandt.com>
2018-09-28 16:39:08 -0300

    Adicionando Carousel e Autoplay

--- site/js/init.js
+++ site/js/init.js
@@ -3,6 +3,18 @@
 
     $('.sidenav').sidenav();
     $('.parallax').parallax();
+    $('.carousel').carousel({
+      fullWidth: true,
+      duration: 500
+    });
+
+    setTimeout(autoplay, 4500);
+    function autoplay() {
+        $('.carousel').carousel('next');
+        setTimeout(autoplay, 3000);
+    }
 
   }); // end of document ready
 })(jQuery); // end of jQuery name space
+
+
